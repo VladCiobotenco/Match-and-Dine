b5793f451f3922e6e6d9733665c9cece578347f7
VladCiobotenco <vlad.ciobotenco@s.unibuc.ro>
2026-04-02 15:49:47 +0300

    Create admin superuser

--- core/backend/admin.py
+++ core/backend/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import UserProfile, Restaurant
 
 # Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('nume', 'prenume', 'email', 'telefon', 'created_at')
+    search_fields = ('nume', 'prenume', 'email')
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('nume', 'adresa', 'email_contact', 'rating', 'created_at')
+    search_fields = ('nume', 'adresa', 'email_contact')
