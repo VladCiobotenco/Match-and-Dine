2d91d82243ffa7c5ce186fe60377af2ec8081f86
VladCiobotenco <vlad.ciobotenco@gmail.com>
2026-04-26 14:50:20 +0300

    Linked overview agent with the frontend

--- core/backend/urls.py
+++ core/backend/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login/', views.api_login, name='api_login'),
     path('register/', views.api_register, name='api_register'),
     path('register-restaurant/', views.api_register_restaurant, name='api_register_restaurant'),
+    path('generate-description/', views.api_generate_description, name='api_generate_description'),
 ]
